eddf7da45096d2961656935395773429c4aa2953
Axel Irribarren <axelirribarren@gmail.com>
2025-11-02 18:55:40 -0300

    agrego comentarios

--- listaSE.cpp
+++ listaSE.cpp
@@ -18,18 +18,38 @@ void agregar(NListaSE * lista, int dato){
     else while(aux -> link != NULL) aux = aux -> link;
     aux -> link = nuevo;
 }
-
 bool eliminar(NListaSE* lista, int dato){
+    //NListaSE de actual, anterior y aux
     NListaSE * actual = lista;
     NListaSE * anterior = NULL;
     NListaSE * aux = NULL;
-
+    
+    //While actual != NULL Y su DATO != DATO, buscando el dato
     while(actual != NULL && actual -> dato != dato){
         anterior = actual;
         actual = actual -> link;
     }
-    
+    //En caso que encuentre al actual y el anterior sea null
+    //Actual se le asigna a aux, lista pasa la siguiente.
+    //eliminas el aux, es verdadero.
+    if(actual != NULL && anterior == NULL){
+        aux = actual;
+        lista = lista -> link;
+        delete aux;
+        return true;
+    }
+    //Si no, anterior es el ultimo, aux se le asigna actual
+    //el siguiente del anterior pasa a ser actual siguiente.
+    //eliminas aux, y retornas verdadero.
+    else if(actual != NULL && anterior != NULL){
+        aux = actual;
+        anterior -> link = actual -> link;
+        delete aux;
+        return true;
+    }
+    return false;
 }
+
 
 int main() {
     
