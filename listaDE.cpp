57c74ed55ff34014fc862c0cb0e93f2be13553aa
Axel Irribarren <axelirribarren@gmail.com>
2025-11-03 00:46:42 -0300

    acomodo

--- listaDE.cpp
+++ listaDE.cpp
@@ -8,16 +8,26 @@ struct NListaDE{
 };
 
 void agregar(NListaDE * &lista,int dato){
+    //CREAMOS EN NODO
     NListaDE * nuevo = new(NListaDE);
     nuevo -> dato = dato;
-
+    //ASIGNAMOS link(NULL) y ant(nuevo)
     nuevo -> link = NULL;
     nuevo -> ant = nuevo;
-
+    //si la lista esta vacia
+    //no hay nada antes del nodo
+    //lista apunta al nuevo
     if(lista == NULL){
         nuevo -> ant = NULL;
         lista = nuevo;
     }
+    //sino esta vacia, creamos un aux
+    //esto nos permite avanzar al ultimo nodo
+    //entonces aux apunta al ultimo nodo
+    //conectamos link y ant
+    //aux -> link = nuevo (Significa que el siguiente
+    //seria el nuevo nodo)
+    //aux -> ant = aux, implica que el anterior era el "ultimo"
     else{
         NListaDE * aux = lista;
         while(aux -> link != NULL){
