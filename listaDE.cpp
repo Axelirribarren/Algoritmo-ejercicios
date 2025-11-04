1f7b7aeccbec7d7fbeec66e053c035b1a4172d8d
Axel Irribarren <axelirribarren@gmail.com>
2025-11-03 23:23:06 -0300

    agrego lista doblemente enlazada

--- listaDE.cpp
+++ listaDE.cpp
@@ -38,7 +38,43 @@ void agregar(NListaDE * &lista,int dato){
     }
 }
 
+bool eliminar(NListaDE * &lista, int dato){
 
+    //Creas la lista
+    NListaDE * actual = lista;
+    //recorres buscando el nodo
+    while(actual != NULL && actual -> dato != dato){
+        actual = actual -> link;
+    }
+    // si no existe, entonces no se encuentra
+    if (actual == NULL){
+        return false;
+    }
+    //Verificamos si es el primero de la lista.
+    if(actual -> ant == NULL){
+        //haces un aux, opcional para comodidad.
+        NListaDE * aux = actual;
+        // asignas el primero
+        lista = actual-> link;
+        //si la lista no es null, entonces,el anterior es null.
+        if(lista != NULL){
+            lista-> ant = NULL;
+        }
+        //lo eliminamos ya que lo encontramos.
+        delete aux; 
+    }
+    else{
+        //sino, en caso de que este en el medio
+        actual -> ant -> link = actual -> link;
+        //excepcion el cual, nos hace ver si es el "ultimo"
+        // ya que sino se rompe todo
+        if(actual -> link != NULL) actual -> link -> ant = actual -> ant;
+        //eliminas el actual, no es necesario usar aux
+        delete actual;
+    }
+    return true;
+
+}
 
 int main() {
     
